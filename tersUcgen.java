0c90f585e2e1279f144ce9c91923c78c501c6457
aysener <30535909+aysener@users.noreply.github.com>
2023-02-05 20:37:35 +0300

    ters üçgen oluşturan java kodu

---
+++ tersUcgen.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class tersUcgen {
+
+	public static void main(String[] args) {
+		Scanner klavye = new Scanner(System.in);
+        System.out.println("Bir Sayý Giriniz :");
+        int n = klavye.nextInt();
+
+        
+        for (int i = n; i >= 0 ; i--) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        
+
+	}
+
+}
